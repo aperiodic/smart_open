9ab06331e880988dca7b33f7f89a1ecce01f53f4
Dan Lidral-Porter <dlidral-porter@cylance.com>
2019-04-15 15:01:26 -0700

    Doctest that doesn't work in Python 2
    Doctest seems to work by comparing the string representations of the
    values, rather than the values themselves, so in Python 2, the three
    lines of this doctest example that have string values all fail because
    e.g. it says that it expects b'Hello, but got 'Hello', even though in
    Python 2 `b'Hello,' == 'Hello,'` is True.

--- smart_open/bytebuffer.py
+++ smart_open/bytebuffer.py
@@ -17,7 +17,31 @@ class ByteBuffer(object):
 
     The bytes are stored in a bytestring, and previously-read bytes are freed
     when the buffer is next filled (by slicing the bytestring into a smaller
-    copy)."""
+    copy).
+
+
+    Example:
+    >>> buf = ByteBuffer(chunk_size = 8)
+    >>> message_bytes = iter([b'Hello, W', b'orld!'])
+    >>> buf.fill(message_bytes)
+    8
+    >>> len(buf) # only chunk_size bytes are filled
+    8
+    >>> buf.peek()
+    b'Hello, W'
+    >>> len(buf) # peek() does not change read position
+    8
+    >>> buf.read(6)
+    b'Hello,'
+    >>> len(buf) # read() does change read position
+    2
+    >>> buf.fill(message_bytes)
+    5
+    >>> buf.read()
+    b' World!'
+    >>> len(buf)
+    0
+    """
 
     def __init__(self, chunk_size=io.DEFAULT_BUFFER_SIZE):
         """Create a ByteBuffer instance that reads chunk_size bytes when filled.
